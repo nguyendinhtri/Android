d72400444c6658435bf9edf10dd1af205f5071ce
nguyendinhtri <ndtri.20it12@vku.udn.vn>
2022-03-15 12:06:20 +0700

    first commit

---
+++ ThiGiuaKi/app/src/main/java/com/example/thigiuaki/ActivityInformation.kt
@@ -0,0 +1,12 @@
+package com.example.thigiuaki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.thigiuaki.R
+
+class ActivityInformation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_information)
+    }
+}
